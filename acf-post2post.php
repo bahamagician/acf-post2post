b86c2ea5c16b9b1ee010151331f424694c752637
SC Graphics <scgraphics@SCs-iMac.local>
2020-02-27 14:02:36 -0500

    change update to add to beginning of relationship

--- acf-post2post.php
+++ acf-post2post.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 	/*
 		Plugin Name: ACF Post-2-Post
@@ -9,24 +9,24 @@
 		Author URI: https://github.com/Hube2
 		GitHub Plugin URI: https://github.com/Hube2/acf-post2post
 		License: GPL v2 or later
-		
-		Automatic Two Way (Bidirectional) Relationships with ACF5 
-		
+
+		Automatic Two Way (Bidirectional) Relationships with ACF5
+
 	*/
-	
+
 	// If this file is called directly, abort.
 	if (!defined('WPINC')) {die;}
-	
+
 	new acf_post2post();
-	
+
 	class acf_post2post {
-		
+
 		public function __construct() {
 			register_activation_hook(__FILE__, array($this, 'activate'));
 			register_deactivation_hook(__FILE__, array($this, 'deactivate'));
 			add_action('after_setup_theme', array($this, 'plugins_loaded'));
 		} // end public function __construct
-		
+
 		public function plugins_loaded() {
 			if (!function_exists('acf_get_setting')) {
 				add_action('admin_init', array($this, 'missing_acf5'));
@@ -40,11 +40,11 @@
 			add_filter('acf/update_value/type=relationship', array($this, 'update_relationship_field'), 11, 3);
 			add_filter('acf/update_value/type=post_object', array($this, 'update_relationship_field'), 11, 3);
 		} // end public function plugins_loaded
-		
+
 		public function missing_acf5() {
 			deactivate_plugins(plugin_basename( __FILE__ ));
 		} // end public function missing_acf5
-		
+
 		public function update_relationship_field($value, $post_id, $field) {
 			$update = true;
 			$update = apply_filters('acf/post2post/update_relationships/key='.$field['key'], $update);
@@ -73,14 +73,16 @@
 				foreach ($previous as $related_id) {
 					if (!in_array($related_id, $new)) {
 						$this->remove_relationship($related_id, $field_name, $post_id);
-						$updated_posts[] = $related_id;
+						// $updated_posts[] = $related_id;
+						array_unshift($updated_posts, $related_id);
 					}
 				}
 			}
 			if (count($new)) {
 				foreach ($new as $related_id) {
 					$this->add_relationship($related_id, $field_name, $post_id);
-					$updated_posts[] = $related_id;
+					// $updated_posts[] = $related_id;
+					array_unshift($updated_posts, $related_id);
 				}
 			}
 			if (count($updated_posts)) {
@@ -88,12 +90,12 @@
 			}
 			return $value;
 		} // end public function update_relationship_field
-		
+
 		private function remove_relationship($post_id, $field_name, $related_id) {
 			/*
 				$post_id = the post id to remove the relationship from
 				$field_name = the field name to update
-				$related_id = the relationship to remove 
+				$related_id = the relationship to remove
 			*/
 			$field = $this->get_field($post_id, $field_name);
 			$array_value = true;
@@ -131,12 +133,12 @@
 			update_post_meta($post_id, '_'.$field_name, $field['key']);
 			do_action('acf/post2post/relationship_updated', $post_id, $field_name, $new_values);
 		} // end private function remove_relationship
-		
+
 		private function add_relationship($post_id, $field_name, $related_id) {
 			/*
 				$post_id = the post id to add the relationship to
 				$field_name = the field name to update
-				$related_id = the relationship to add 
+				$related_id = the relationship to add
 			*/
 			$field = $this->get_field($post_id, $field_name);
 			if (!$field) {
@@ -165,14 +167,15 @@
 			$value = array_map('intval', $value);
 			if (($max_posts == 0 || count($value) < $max_posts) &&
 					!in_array($related_id, $value)) {
-				$value[] = $related_id;
+				// $value[] = $related_id;
+				array_unshift($value, $related_id);
 			} elseif ($max_posts > 0) {
 				$overwrite_settings = apply_filters('acf-post2post/overwrite-settings', array());
-				if (isset($overwrite_settings[$field_name]) && 
+				if (isset($overwrite_settings[$field_name]) &&
 					isset($overwrite_settings[$field_name]['overwrite']) &&
 					$overwrite_settings[$field_name]['overwrite']) {
 					$type = 'first';
-					if (isset($overwrite_settings[$field_name]['type']) && 
+					if (isset($overwrite_settings[$field_name]['type']) &&
 							in_array(strtolower($overwrite_settings[$field_name]['type']), array('first', 'last'))) {
 						$type = strtolower($overwrite_settings[$field_name]['type']);
 					}
@@ -183,7 +186,8 @@
 					}
 					// remove this relationship from the post that was just removed
 					$this->remove_relationship(intval($remove), $field_name, $post_id);
-					$value[] = $related_id;
+					// $value[] = $related_id;
+					array_unshift($value, $related_id);
 				} // end field overwrite
 			} // end if else
 			if (!$array_value) {
@@ -195,7 +199,7 @@
 			update_post_meta($post_id, '_'.$field_name, $field['key']);
 			do_action('acf/post2post/relationship_updated', $post_id, $field_name, $value);
 		} // end private function add_relationship
-		
+
 		public function get_field($post_id, $field_name) {
 			$field = false;
 			$found = false;
@@ -224,7 +228,7 @@
 			wp_cache_set($cache_key, $field, 'acfpost2post');
 			return $field;
 		} // end public function get_field
-		
+
 		public function post_field_groups($post_id) {
 			$found = false;
 			$cache = wp_cache_get('post_field_groups-'.$post_id, 'acfpost2post', false, $found);
@@ -240,15 +244,15 @@
 			wp_cache_set('post_field_groups-'.$post_id, $field_groups, 'acfpost2post');
 			return $field_groups;
 		} // end public function post_field_groups
-		
+
 		public function activate() {
 			// just in case I need to do something to activate
 		} // end public function activate
-		
+
 		public function deactivate() {
 			// just in case I need to do something to deactivate
 		} // end public function deactivate
-		
+
 	} // end class acf_post2post
 
 ?>
